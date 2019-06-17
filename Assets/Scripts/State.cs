d25d16a900d786362874ea8032ba8c0c2f396018
Rinita Nair <rnair3@uncc.edu>
2019-06-17 01:59:38 -0400

    First Commit

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    private string name;
+    [TextArea (10,34)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+}
---
+++ Assets/Scripts/TextsPortion.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextsPortion : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI story;
+    [SerializeField] State startingState;
+    [SerializeField] GameObject alert;
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        story.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+
+        for(int i = 0; i < nextStates.Length; i++)
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                state = nextStates[i];
+            }
+            //if (state.GetName().Equals("Game Over"))
+            //{
+            //    if (Input.GetKeyDown(KeyCode.Q))
+            //    {
+            //        Application.Quit();
+            //    }
+            //}
+        }
+
+
+        //if (state.GetName().Equals("Game Over"))
+        //{
+        //    if (Input.GetKeyDown(KeyCode.Q))
+        //    {
+        //        Application.Quit();
+        //    }
+        //}
+
+        story.text = state.GetStateStory();
+
+    }
+}
